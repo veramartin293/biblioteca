5f8c2561aad07c402a56684dd9e81c421ff1b880
Andre Martin Vera Valdez <andre.vera@banregio.com>
2022-06-22 17:49:24 -0600

    estructura principal de componentes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,27 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CategoriasComponent } from './components/categorias/categorias.component';
+import { LibrosComponent } from './components/libros/libros.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    redirectTo: '/libros',
+    pathMatch: 'full'
+  },
+  {
+    path: 'libros',
+    component: LibrosComponent
+  },
+  {
+    path: 'categorias',
+    component: CategoriasComponent
+  },
+  {
+    path: '**',
+    redirectTo: '/libros'
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LibrosComponent } from './components/libros/libros.component';
+import { CategoriasComponent } from './components/categorias/categorias.component';
+import { MenuComponent } from './components/menu/menu.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LibrosComponent,
+    CategoriasComponent,
+    MenuComponent
   ],
   imports: [
     BrowserModule,
